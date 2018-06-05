97e99bb345bd848b9c873a0a5594525bf63da049
[Alex Brooks] <alexanderathome@btinternet.com>
2018-06-05 12:25:13 +0100

    pliz push

--- js/messaging.js
+++ js/messaging.js
@@ -1,23 +1,3 @@
-var database = firebase.database();
-var chatRef = firebase.database().ref().child("messages");
-chatRef.on("child_added", snap => {
-  var chats = snap.child("message").val();
-  $("#mainChat").append(chats + "<p>\n</p>")
-});
-
-
-/*chat.on('value', function(datasnapshot) {
-  mainChat.innerText = datasnapshot.val();
-});*/
-
-function addMessage() {
-  var inputMessage = document.getElementById("input").value;
-  firebase.database().ref().child("messages").push().set({
-    message: inputMessage
-  });
-  console.log("added to database");
-}
-
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
@@ -29,8 +9,33 @@ firebase.auth().onAuthStateChanged(function(user) {
     var uid = user.uid;
     var providerData = user.providerData;
     console.log(displayName);
+    return displayName;
   } else {
     // User is signed out.
     console.log("user is logged out");
   }
 });
+
+
+var database = firebase.database();
+var chatRef = firebase.database().ref().child("messages");
+chatRef.on("child_added", snap => {
+  var chats = snap.child("message").val();
+  var username = snap.child("user").val();
+  $("#mainChat").append(username + "<p>: </p>" + chats + "<p>\n</p>")
+});
+
+
+/*chat.on('value', function(datasnapshot) {
+  mainChat.innerText = datasnapshot.val();
+});*/
+
+function addMessage() {
+  var inputMessage = document.getElementById("input").value;
+  var name = firebase.auth().currentUser;
+  firebase.database().ref().child("messages").push().set({
+    message: inputMessage,
+    user: name
+  });
+  console.log("added to database");
+}
