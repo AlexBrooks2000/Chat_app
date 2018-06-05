ca7485ac43e43427e5ee85bff1835f9a2b85bcd0
[Alex Brooks] <alexanderathome@btinternet.com>
2018-06-05 11:50:31 +0100

    stay logged in

--- js/messaging.js
+++ js/messaging.js
@@ -6,9 +6,9 @@ chatRef.on("child_added", snap => {
 });
 
 
-chat.on('value', function(datasnapshot) {
+/*chat.on('value', function(datasnapshot) {
   mainChat.innerText = datasnapshot.val();
-});
+});*/
 
 function addMessage() {
   var inputMessage = document.getElementById("input").value;
@@ -17,3 +17,20 @@ function addMessage() {
   });
   console.log("added to database");
 }
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    console.log(displayName);
+  } else {
+    // User is signed out.
+    console.log("user is logged out");
+  }
+});
